02d2b6199eecbd97e8c9f141dd39e0f39ad17856
Bozdog Ioana <bozdogioana@yahoo.com>
2021-08-25 17:19:56 +0300

    Add a lot of stuff

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -16,14 +16,14 @@ function Coin({ front, back, text }: CoinProps) {
   return (
     <div className={style.circleContainer}>
       <div className={style.circle}>
-        <figure className={style.circleFront} style={{position: 'absolute', margin: 0 }}>
+        <figure className={style.circleFront} style={{ position: 'absolute', margin: 0 }}>
           <img alt="ts_js" src={front} />
         </figure>
-        <figure className={style.circleBack} style={{position: 'absolute', margin: 0 }}>
+        <figure className={style.circleBack} style={{ position: 'absolute', margin: 0 }}>
           <div style={{ position: 'relative', textAlign: 'center' }}>
             <img alt="circle_purple" src={back} />
             <p style={{
-              position: 'absolute', fontSize: font_size, width: '5vw', height: '5vw',
+              position: 'absolute', fontSize: window.innerWidth > 420 ? font_size : '3vw', width: window.innerWidth > 420 ? '5vw' : '11vw', height: window.innerWidth > 420 ? '5vw' : '11vw',
               margin: 0, top: position_top, left: position_left, transform: 'translate(-50%, -50%)',
               color: 'rgb(27, 33, 112)'
             }}>{text}</p>
